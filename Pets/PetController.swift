fc692459f5d451f38749b475249680c4386a4658
Ilqar Ilyasov <imilqar@gmail.com>
2018-08-28 15:55:41 -0500

    Added PetController functions

--- Pets/PetController.swift
+++ Pets/PetController.swift
@@ -12,4 +12,24 @@ class PetController {
     
     private(set) var pets: [Pet] = []
     
+    func createNewPet(name: String, age: Int) {
+        
+        let newPet = Pet(name: name, age: age)
+        pets.append(newPet)
+        
+    }
+    
+    func updatePet(newPet: Pet, name: String, age: Int) {
+        
+        guard let index = pets.index(of: newPet) else { return }
+        var updatedPet = newPet
+        
+        updatedPet.name = name
+        updatedPet.age = age
+        
+        pets.remove(at: index)
+        pets.insert(newPet, at: index)
+        
+    }
+    
 }
